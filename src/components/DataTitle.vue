8bb8f0aa26b8c37f36e1512be0441a20160af7d0
Karem Shabana <51063078+KMS74@users.noreply.github.com>
2021-06-20 01:10:04 +0200

    update data title

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="text-center">
     <h2 class="text-3xl font-bold">{{ text }}</h2>
-    <div class="text-2xl mt-4 mb-10">
-      {{ timestamp }}
-    </div>
+    <div class="text-2xl mt-4 mb-10">Last update:- {{ timestamp }}</div>
   </div>
 </template>
 
